ecd37048bfb0e18e040a5060a7a8a171a47630a0
jaeik92lee <jaeik92lee@gmail.com>
2019-02-20 00:35:02 +0900

    init boot

---
+++ src/main/java/com/samsung/tech/gongnam/GongnamApplication.java
@@ -0,0 +1,13 @@
+package com.samsung.tech.gongnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongnamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GongnamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samsung/tech/gongnam/GongnamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.samsung.tech.gongnam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GongnamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
