8c80a49af66eb473e71433b28cf0ad4c646bbd94
jaeik92lee <jaeik92lee@gmail.com>
2019-02-20 23:15:17 +0900

    p.92 - 중복 제거

--- src/main/java/com/samsung/tech/gongnam/dao/factory/DaoFactory.java
+++ src/main/java/com/samsung/tech/gongnam/dao/factory/DaoFactory.java
@@ -3,30 +3,24 @@ package com.samsung.tech.gongnam.dao.factory;
 import com.samsung.tech.gongnam.dao.UserDao;
 import com.samsung.tech.gongnam.dao.connection.ConnectionMaker;
 import com.samsung.tech.gongnam.dao.connection.impl.DConnectionMaker;
-import com.samsung.tech.gongnam.dao.connection.impl.NConnectionMaker;
 import com.samsung.tech.gongnam.dao.partner.DUserDao;
 import com.samsung.tech.gongnam.dao.partner.NUserDao;
 
 public class DaoFactory {
 
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-
-        return userDao;
+        return new UserDao(connectionMaker());
     }
 
     public NUserDao nUserDao() {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-        NUserDao nUserDao = new NUserDao(connectionMaker);
-
-        return nUserDao;
+        return new NUserDao(connectionMaker());
     }
 
     public DUserDao dUserDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        DUserDao dUserDao = new DUserDao(connectionMaker);
+        return new DUserDao(connectionMaker());
+    }
 
-        return dUserDao;
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
